f8149352bd76a8758dda98dffb8639e06d078923
Max <lxghoney@163.com>
2017-11-02 15:16:15 +0800

    remove neo4j,fileserver model

--- neo4j/src/main/java/com/asiainfo/neo4j/repositories/GoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asiainfo.neo4j.repositories;
-
-import com.asiainfo.neo4j.domain.Goods;
-import org.springframework.data.neo4j.repository.GraphRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GoodsRepository extends GraphRepository<Goods> {
-}
