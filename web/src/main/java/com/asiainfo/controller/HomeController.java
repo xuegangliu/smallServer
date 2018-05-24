197cc8f3e053e50c7f6005def690344264aabd4c
Max <lxghoney@163.com>
2018-05-25 01:22:30 +0800

    modify properties db

---
+++ web/src/main/java/com/asiainfo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.asiainfo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: xuegangliu
+ * @date: 5/24/2018 8:10 PM
+ * @DES:
+ * @version: v1.0
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping(name = "/index",method = RequestMethod.GET)
+    public String home(){
+        return "hello";
+    }
+}
